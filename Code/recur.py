3a8cedeccb6cc1c089aae977b44ba210635252e4
gcallah <gcallah@mac.com>
2017-10-27 09:10:33 -0400

    Extended stack depth.

--- Code/recur.py
+++ Code/recur.py
@@ -1,54 +1,58 @@
 #!/usr/env/bin python3
+import sys
+
+MAX_RECURS = 5000
+sys.setrecursionlimit(10000) 
 
 ERROR = -1
 memo = []
 NOT_SEEN = -999999
-MAX_RECURS = 501  # because van Rossum doesn't "like" recursion, we got dis
-                  # dumb limit on stack depth. It can be extended if you need
-                  # it!
 
 # what follows are some recurrences for you to try:
 
 # Fibonacci series:
 fibb = {0: 0, 1: 1}
-def fibf(n):
-    return recur(n - 1, fibb, fibf) + recur(n - 2, fibb, fibf)
+def fibf(n, bases):
+    return recur(n - 1, bases, fibf) + recur(n - 2, bases, fibf)
 # call like this:
 #    recur(500, fibb, fibf, True) -- returns 500th Fibonacci number
 
 # Lucas series:
 lucb = {0: 2, 1: 1}
-def lucf(n):
-    return recur(n - 1, lucb, lucf) + recur(n - 2, lucb, lucf)
+# just use fibonacci function with Lucas bases!
 # call like this:
-#    recur(500, lucb, lucf, True) -- returns 500th Lucas number
+#    recur(500, lucb, fibf, True) -- returns 500th Lucas number
 
 # geometric series:
-RATIO = 2
-geob = {0: 1, 1: RATIO}  # redefine RATIO to get a different multiple
-def geof(n):
-    return geob[1] * recur(n - 1, geob, geof)
+RATIO = 2    # redefine RATIO to get a different multiple
+geob = {0: 1, 1: RATIO}
+def geof(n, bases):
+    return geob[1] * recur(n - 1, bases, geof)
 # call like this:
 #    recur(500, geob, geof, True) -- returns 500th power of 2
 
 # arithmetic series:
-STEP = 4
-arib = {0: 0, 1: STEP}  # redefine STEP to get a different step size
-def arif(n):
-    return arib[1] + recur(n - 1, arib, arif)
+STEP = 4     # redefine STEP to get a different step size
+arib = {0: 0, 1: STEP}
+def arif(n, bases):
+    return arib[1] + recur(n - 1, bases, arif)
 # call like this:
 #    recur(500, arib, arif, True) -- returns 500th number counting by 4s
 
 # the value of an annuity with equal contributions each year
 #  and interest compounded annually:
 INT_RATE = .05
-INITIAL = 1000
-compb = {0: INITIAL}  # redefine INITIAL to get different yearly amount
-
-def compf(n):
-    return compb[0] + recur(n - 1) * (1 + INT_RATE)
+INITIAL = 1000     # redefine INITIAL to get different yearly amount
+compb = {0: INITIAL}
+def compf(n, bases):
+    return compb[0] + recur(n - 1, bases, compf) * (1 + INT_RATE)
 # call like this:
 #    recur(20, compb, compf, True) -- value of annuity after 20 years
+
+# an arbitrary recurrence:
+arbb = {0: 1.01, 1: .98, 2: .88, 3: 1.1}
+def arbf(n, bases):
+    return recur(n - 1, bases, arbf) * recur(n - 4, bases, arbf)
 
 def recur(n, bases, f, init=False):
     global memo
@@ -65,5 +69,5 @@ def recur(n, bases, f, init=False):
     elif memo[n] != NOT_SEEN:
         return memo[n]
     else:
-        memo[n] = f(n)
+        memo[n] = f(n, bases)
         return memo[n]
