ad0544fb759eb2a42de5aba79ba19a59a030ec87
gcallah <gcallah@mac.com>
2018-08-09 18:04:10 -0400

    Formatting mostly working.

--- Code/pascals.py
+++ Code/pascals.py
@@ -1,7 +1,44 @@
-SPACES_PER_ENTRY = 5
+#!/usr/bin/python
 
-def output_triangle(levels = 10, m = 1):
-    width = (levels + 1) * SPACES_PER_ENTRY
+SPACES_PER_ENTRY = 6
+
+trows= []
+
+def format_row(row):
+    s = ""
+    for num in row:
+        n = str(num)
+        pad = 0
+        if len(n) < SPACES_PER_ENTRY:
+            pad = SPACES_PER_ENTRY - len(n)
+        s += str(num) + (" " * pad)
+    return s
+
+
+def create_triangle(rows=10, m=1):
+    prow = None
+    for row in range(0, rows + 1):
+        print("Calculating row " + str(row))
+        entries = row + 1
+        nrow = []
+        trows.append(nrow)
+        for k in range(0, entries):
+            if (k == 0) or (k == entries - 1):
+                nrow.append(1)
+            else:
+                nrow.append(prow[k - 1] + prow[k])
+        prow = nrow
+
+
+def output_triangle():
+    num_rows = len(trows)
+    row_width = (num_rows + 1) * SPACES_PER_ENTRY
+    for row in trows:
+        padamt = row_width - (len(row) * SPACES_PER_ENTRY) / 2
+        padding = " " * padamt
+        print(padding + format_row(row) + padding)
+
 
 if __name__ == "__main__":
-    pass
+    create_triangle()
+    output_triangle()
