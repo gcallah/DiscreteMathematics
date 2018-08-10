78019b7f3b70b91526d35562c199b0d0cf71da11
gcallah <gcallah@mac.com>
2018-08-10 01:39:27 -0400

    Added mod and num rows.

--- Code/pascals.py
+++ Code/pascals.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-SPACES_PER_ENTRY = 6
+from sys import argv
+
+sp_per_entry = 5
 
 trows= []
 
@@ -9,13 +11,13 @@ def format_row(row):
     for num in row:
         n = str(num)
         pad = 0
-        if len(n) < SPACES_PER_ENTRY:
-            pad = SPACES_PER_ENTRY - len(n)
+        if len(n) < sp_per_entry:
+            pad = sp_per_entry - len(n)
         s += str(num) + (" " * pad)
     return s
 
 
-def create_triangle(rows=10, m=1):
+def create_triangle(rows=10, m=None):
     prow = None
     for row in range(0, rows + 1):
         print("Calculating row " + str(row))
@@ -26,19 +28,32 @@ def create_triangle(rows=10, m=1):
             if (k == 0) or (k == entries - 1):
                 nrow.append(1)
             else:
-                nrow.append(prow[k - 1] + prow[k])
+                k = prow[k - 1] + prow[k]
+                if m is not None:
+                    k %= m
+                nrow.append(k)
         prow = nrow
 
 
 def output_triangle():
     num_rows = len(trows)
-    row_width = (num_rows + 1) * SPACES_PER_ENTRY
+    row_width = (num_rows + 1) * sp_per_entry
     for row in trows:
-        padamt = row_width - (len(row) * SPACES_PER_ENTRY) / 2
+        padamt = row_width - (len(row) * sp_per_entry) / 2
         padding = " " * padamt
         print(padding + format_row(row) + padding)
 
 
 if __name__ == "__main__":
-    create_triangle()
+    mod = None
+    rows = 10
+    if len(argv) > 1:
+        rows = int(argv[1])
+    if len(argv) > 2:
+        mod = int(argv[2])
+    if rows > 12 and mod is None:
+        sp_per_entry += 1
+    if rows > 19 and mod is None:
+        sp_per_entry += 1
+    create_triangle(rows, mod)
     output_triangle()
